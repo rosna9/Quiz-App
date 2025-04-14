6c2b992a38065f7a286bdadcf02f590f68fc1a31
rosna9 <rosnaranipradhan18@gmail.com>
2025-04-14 17:01:06 +0530

    some changes

--- src/App.jsx
+++ src/App.jsx
@@ -75,7 +75,7 @@ function App() {
           </div>
           {/* Timer */}
           <div className="bg-blue-100 px-4 py-2 rounded-lg text-blue-800 font-semibold shadow">
-            ⏳ Time Left: {timeLeft}s
+             ⏳ Time Left: {timeLeft}s
           </div>
         </div>
 
